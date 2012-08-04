468fc2f063780c463769c7dd438e7e5b3090397d
blaskred <detso@rambler.ru>
2012-08-04 23:56:13 +0600

    1

--- src/server/game/Entities/Item/Item.cpp
+++ src/server/game/Entities/Item/Item.cpp
@@ -1340,3 +1340,131 @@ void Item::RemoveFakeDisplay()
 {
     SetItemDisplayId(0);
 }
+
+FakeResult Item::SetFakeDisplay(uint32 iEntry)
+{
+    if (!iEntry)
+    {
+        RemoveFakeDisplay();
+        return FAKE_ERR_OK;
+    }
+
+    ItemTemplate const* myTmpl    = GetTemplate();
+    ItemTemplate const* otherTmpl = sObjectMgr->GetItemTemplate(iEntry);
+
+    if (!otherTmpl)
+        return FAKE_ERR_CANT_FIND_ITEM;
+
+    bool diff_inv_type = false;
+    if (myTmpl->InventoryType != otherTmpl->InventoryType)
+    {
+        diff_inv_type = true;
+
+        if (myTmpl->InventoryType == INVTYPE_CHEST && otherTmpl->InventoryType == INVTYPE_ROBE)
+            diff_inv_type = false;
+
+        if (myTmpl->InventoryType == INVTYPE_ROBE && otherTmpl->InventoryType == INVTYPE_CHEST)
+            diff_inv_type = false;
+
+        if (myTmpl->InventoryType == INVTYPE_RANGED && otherTmpl->InventoryType == INVTYPE_RANGEDRIGHT)
+            diff_inv_type = false;
+
+        if (myTmpl->InventoryType == INVTYPE_RANGEDRIGHT && otherTmpl->InventoryType == INVTYPE_RANGED)
+            diff_inv_type = false;
+    }
+
+    if (diff_inv_type)
+        return FAKE_ERR_DIFF_SLOTS;
+
+    switch (myTmpl->InventoryType)
+    {
+        case INVTYPE_NON_EQUIP:
+        //case INVTYPE_HEAD:
+        //case INVTYPE_NECK:
+        //case INVTYPE_SHOULDERS:
+        case INVTYPE_BODY:
+        //case INVTYPE_CHEST:
+        //case INVTYPE_WAIST:
+        //case INVTYPE_LEGS:
+        //case INVTYPE_FEET:
+        //case INVTYPE_WRISTS:
+        //case INVTYPE_HANDS:
+        //case INVTYPE_FINGER:
+        case INVTYPE_TRINKET:
+        //case INVTYPE_WEAPON:
+        //case INVTYPE_SHIELD:
+        //case INVTYPE_RANGED:
+        //case INVTYPE_CLOAK:
+        //case INVTYPE_2HWEAPON:
+        case INVTYPE_BAG:
+        case INVTYPE_TABARD:
+        //case INVTYPE_ROBE:
+        //case INVTYPE_WEAPONMAINHAND:
+        //case INVTYPE_WEAPONOFFHAND:
+        //case INVTYPE_HOLDABLE:
+        case INVTYPE_AMMO:
+        case INVTYPE_THROWN:
+        //case INVTYPE_RANGEDRIGHT:
+        case INVTYPE_QUIVER:
+        case INVTYPE_RELIC:
+            return FAKE_ERR_DIFF_SLOTS;
+        default:
+            break;
+    }
+
+    if (myTmpl->Class != otherTmpl->Class)
+        return FAKE_ERR_DIFF_CLASS;
+
+    bool diff_sub_class = false;
+    if (myTmpl->SubClass != otherTmpl->SubClass)
+    {
+        diff_sub_class = true;
+        if (myTmpl->Class == ITEM_CLASS_WEAPON)
+        {
+            //BOW & CROSSBOW
+            if (myTmpl->SubClass == ITEM_SUBCLASS_WEAPON_BOW && otherTmpl->SubClass == ITEM_SUBCLASS_WEAPON_CROSSBOW)
+                diff_sub_class = false;
+
+            if (myTmpl->SubClass == ITEM_SUBCLASS_WEAPON_CROSSBOW && otherTmpl->SubClass == ITEM_SUBCLASS_WEAPON_BOW)
+                diff_sub_class = false;
+
+            //BOW & GUN
+            if (myTmpl->SubClass == ITEM_SUBCLASS_WEAPON_BOW && otherTmpl->SubClass == ITEM_SUBCLASS_WEAPON_GUN)
+                diff_sub_class = false;
+
+            if (myTmpl->SubClass == ITEM_SUBCLASS_WEAPON_GUN && otherTmpl->SubClass == ITEM_SUBCLASS_WEAPON_BOW)
+                diff_sub_class = false;
+
+            //CROSSBOW & GUN
+            if (myTmpl->SubClass == ITEM_SUBCLASS_WEAPON_CROSSBOW && otherTmpl->SubClass == ITEM_SUBCLASS_WEAPON_GUN)
+                diff_sub_class = false;
+
+            if (myTmpl->SubClass == ITEM_SUBCLASS_WEAPON_GUN && otherTmpl->SubClass == ITEM_SUBCLASS_WEAPON_CROSSBOW)
+                diff_sub_class = false;
+        }
+    }
+
+    if (diff_sub_class)
+        return FAKE_ERR_DIFF_CLASS;
+
+    if (myTmpl->Class == ITEM_CLASS_WEAPON && myTmpl->SubClass == ITEM_SUBCLASS_WEAPON_FISHING_POLE)
+        return FAKE_ERR_DIFF_CLASS;
+
+    if (otherTmpl->Class == ITEM_CLASS_WEAPON && otherTmpl->SubClass == ITEM_SUBCLASS_WEAPON_FISHING_POLE)
+        return FAKE_ERR_DIFF_CLASS;
+
+    if (otherTmpl->Quality == ITEM_QUALITY_LEGENDARY || otherTmpl->Quality == ITEM_QUALITY_POOR)
+        return FAKE_ERR_WRONG_QUALITY;
+
+    if (myTmpl->Quality == ITEM_QUALITY_LEGENDARY || myTmpl->Quality == ITEM_QUALITY_POOR)
+        return FAKE_ERR_WRONG_QUALITY;
+
+    SetItemDisplayId(iEntry);
+
+    return FAKE_ERR_OK;
+}
+
+void Item::RemoveFakeDisplay()
+{
+    SetItemDisplayId(0);
+}
