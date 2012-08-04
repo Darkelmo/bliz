8d303773045b36969a4298a7ff89d921907c2564
blaskred <detso@rambler.ru>
2012-08-04 22:39:49 +0600

    updatecore

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -24,6 +24,8 @@ void AddSC_example_escort();
 void AddSC_example_gossip_codebox();
 void AddSC_example_misc();
 void AddSC_example_commandscript();
+// Transmogrify
+void AddSC_transmogrify_script();
 
 //TeleNPC2
 void AddSC_npc_teleport();
@@ -1287,6 +1289,7 @@ void AddBattlegroundScripts()
 
 void AddCustomScripts()
 {
+    AddSC_transmogrify_script();
 #ifdef SCRIPTS
     /* This is where custom scripts should be added. */
     //TeleNPC2
